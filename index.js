05d6b7fb08c0cdb910df12b5d5fc598826e9a38f
Douglas Eskelius <douglas.eskelius@gmail.com>
2020-06-18 08:32:29 +0200

    set up standard config and boilerplate code

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 const express = require('express');
 const socketIO = require('socket.io');
+const http = require('http');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+const PORT = process.env.PORT || 8080;
+
+io.origins('*:*');
+
+io.on('connection', (socket) => {
+  console.log(socket);
+});
+
+server.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
